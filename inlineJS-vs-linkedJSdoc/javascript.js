128c78494fd7a5bf663c18380a81a4ca271caaa4
rugwall01 <161780368+Rugwall01@users.noreply.github.com>
2024-09-22 12:04:33 -0700

    created inlineJS-vs-linkedJSdoc directory, made two versions of index and styles docs and one JS doc to compare scripting JS directy into an html file vs linking it like stylesheet

---
+++ inlineJS-vs-linkedJSdoc/javascript.js
@@ -0,0 +1,122 @@
+let name = "Justin";
+let surname = "Long";
+let food = "Cheese";
+
+console.log(name);
+console.log(surname);
+console.log(food);
+console.log(name);
+console.log(surname);
+console.log((3 + 2) - 76 * (1 + 1));
+console.log(4+4+5+7+8+19+17);
+console.log((4+6+9)/77);
+let a = 10;
+console.log(a);
+console.log(9 * a)
+let b = 7 * a;
+console.log(b);
+
+const max = 57;
+const actual = max-13;
+const percentage = actual/max
+
+console.log(percentage)
+
+let c;
+
+a = b = c = 2 + 2;
+
+
+console.log(a)
+
+c = 2 + 2;
+b = c;
+a = b;
+
+console.log(a)
+
+
+let n = 2;
+n = n + 5;
+n = n * 2;
+
+
+console.log(n)
+
+n = 2;
+n += 5;
+n *= 2;
+console.log(n)
+
+++n;
+console.log(n);
+
+n = ++n * 2;
+console.log(n);
+
+n *= ++n * 2;
+console.log(n);
+
+let x = 100/"Apple";
+
+console.log(x);
+
+let mynum = 2
+while(mynum != Infinity) {
+    mynum *= mynum
+}
+
+console.log(mynum);
+
+let k = 2/0;
+let m = -2/0
+
+console.log(k, m)
+
+
+console.log(typeof Infinity);
+
+let w = 0xAA
+console.log(w);
+
+console.log(10**308)
+
+console.log(0x72)
+
+let
+const
+var
+if
+else 
+console.{insert}
+alert
+
+Number
+BigInt
+String
+Boolean
+Object
+Symbol
+Null
+undefined
+
+=
+==
+===
+!=
+>
+<
+
+|| 
+&&
+
++
+-
+*
+/
+**
+++
+--
+
+/* to be continued */
+
