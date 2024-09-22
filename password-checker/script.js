31de0b234e48cee0ad919e0efb5322b5a50d6d0f
rugwall01 <161780368+Rugwall01@users.noreply.github.com>
2024-09-22 15:40:02 -0700

    Fixed bugs in if statements and removed unneccessary function, left example of function in the notes for future reference

--- password-checker/script.js
+++ password-checker/script.js
@@ -4,21 +4,18 @@ if(user === "Admin") {
     let password = prompt("Please enter password");
     if(password === "TheMaster") {
         alert("Welcome!!");
-    } else if(password === "" || function keyPress (e) {
+    } else if(password === "" || password === null /* This was unnecessary --> function keyPress (e) {
         if(e.key === "Escape") {
             alert("Canceled");
         }
-    }) {
-       //alert("Canceled");
+    }*/) {
+       alert("Canceled");
     } else {
         alert("Wrong Password!")
     }
-} else if(user === "" || null || function keyPress (e) {
-    if(e.key === "Escape"){
-        
-    }}) {
-   //alert("Canceled");
-} else if(user !== "Admin") {
+} else if(user === "" || user === null) {
+   alert("Canceled");
+} else {
     alert("I don't know you");
 }
 
