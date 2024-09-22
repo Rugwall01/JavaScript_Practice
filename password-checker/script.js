e91bc47814a0a53add78bab5f757e27ecddf1ec0
rugwall01 <161780368+Rugwall01@users.noreply.github.com>
2024-09-22 15:28:46 -0700

    draft 2 of code for password checker. Need to debug fuction keypress e and else statement(currently 'else if' number 2)

--- password-checker/script.js
+++ password-checker/script.js
@@ -1,20 +1,24 @@
 let user = prompt("Who is there?");
-/*let cancel = alert("Canceled");
-let password = prompt("Please enter password");
-let other = alert("I don't know you");*/
 
-function keyPress (e) {
-    if(e.key === "Escape") {
-        alert("Canceled");
+if(user === "Admin") {
+    let password = prompt("Please enter password");
+    if(password === "TheMaster") {
+        alert("Welcome!!");
+    } else if(password === "" || function keyPress (e) {
+        if(e.key === "Escape") {
+            alert("Canceled");
+        }
+    }) {
+       //alert("Canceled");
+    } else {
+        alert("Wrong Password!")
     }
-}
-
-
-if (user === "Admin") {
-    prompt("Please enter password");
-} else if(user == "" || null) {
-    alert("Canceled");
-} else {
+} else if(user === "" || null || function keyPress (e) {
+    if(e.key === "Escape"){
+        
+    }}) {
+   //alert("Canceled");
+} else if(user !== "Admin") {
     alert("I don't know you");
 }
 
